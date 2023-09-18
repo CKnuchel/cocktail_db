ff063a05345200ad53e11311d6be65b0e88933a5
Christoph Knuchel <119663666+CKnuchel@users.noreply.github.com>
2023-09-18 11:57:32 +0200

    Vereinfachte Funktionen

--- src/pages/Cocktail.js
+++ src/pages/Cocktail.js
@@ -3,19 +3,37 @@ import '../css/cocktail.css';
 
 export default function Cocktail() {
     const [cocktailData, setCocktailData] = useState(null);
+    const [cocktailName, setCocktailName] = useState('');
 
-    async function getCocktailInfosAsJSON() {
-        const searchName = document.getElementById('cocktailName').value;
-        const response = await fetch(
-            `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchName}`
-        );
-        const data = await response.json();
+    async function fetchCocktailData() {
+        try {
+            const response = await fetch(
+                `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktailName}`
+            );
+            const data = await response.json();
 
-        if (data.drinks === null) {
-            alert('Kein Cocktail mit diesem Namen gefunden');
-        } else {
-            setCocktailData(data.drinks[0]);
+            if (data.drinks && data.drinks.length > 0) {
+                setCocktailData(data.drinks[0]);
+            } else {
+                alert('Kein Cocktail mit diesem Namen gefunden');
+            }
+        } catch (error) {
+            console.error('Fehler beim Abrufen der Daten:', error);
         }
+    }
+
+    function renderIngredients() {
+        const ingredients = [];
+        for (let i = 1; i <= 15; i++) {
+            const ingredient = cocktailData[`strIngredient${i}`];
+            const measure = cocktailData[`strMeasure${i}`];
+            if (ingredient && measure) {
+                ingredients.push(`${measure} ${ingredient}`);
+            }
+        }
+        return ingredients.map((ingredient, index) => (
+            <li key={index}>{ingredient}</li>
+        ));
     }
 
     return (
@@ -24,14 +42,18 @@ export default function Cocktail() {
                 <h1>Cocktail DB</h1>
             </div>
             <div className="searchBar">
-                <input id="cocktailName" type="text" placeholder="Cocktailname" />
-                <button onClick={getCocktailInfosAsJSON}>Suchen</button>
+                <input
+                    type="text"
+                    placeholder="Cocktailname"
+                    value={cocktailName}
+                    onChange={(e) => setCocktailName(e.target.value)}
+                />
+                <button onClick={fetchCocktailData}>Suchen</button>
             </div>
             {cocktailData && (
                 <div className="infos">
                     <div className="cocktail-img">
                         <img
-                            id="cocktailImg"
                             src={cocktailData.strDrinkThumb}
                             alt={cocktailData.strDrink}
                             width="400"
@@ -40,21 +62,11 @@ export default function Cocktail() {
                     </div>
                     <div className="cocktail-ingredients">
                         <h3>Zutaten</h3>
-                        <ul id="ingredients">
-                            {[...Array(15).keys()].map((i) => {
-                                const ingredient = cocktailData[`strIngredient${i + 1}`];
-                                const measure = cocktailData[`strMeasure${i + 1}`];
-                                return ingredient && measure ? (
-                                    <li key={i}>
-                                        {measure} {ingredient}
-                                    </li>
-                                ) : null;
-                            })}
-                        </ul>
+                        <ul>{renderIngredients()}</ul>
                     </div>
                     <div className="cocktail-instructions">
                         <h3>Zubereitung</h3>
-                        <p id="instructions">{cocktailData.strInstructionsDE}</p>
+                        <p>{cocktailData.strInstructionsDE}</p>
                     </div>
                 </div>
             )}
