8096e21aac1bcce83f7685598a6b8f1fc7baa90e
Christoph Knuchel <119663666+CKnuchel@users.noreply.github.com>
2023-09-18 10:19:15 +0200

    ADDED: Home + CSS

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,16 @@
+import '../css/home.css';
+
+import homeImage from '../images/home.png'
+
 export default function Cocktail() {
     return (
-        <div>
+        <div className='home'>
+        <div className='titel'>
             <h1>Cocktail DB</h1>
+        </div>
+        <div className='home-image'>
+            <img src= {homeImage} alt='Cocktail' />
+        </div>
         </div>
     )
 }
